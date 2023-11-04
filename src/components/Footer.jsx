5479d50c054fc87e1a3822f5d51b9e5b7b5bd799
zouig mohamed <zouig.mohamed@gmail.com>
2023-11-04 19:43:56 +0100

    year footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
+const  getCurrentYear = ()=>  {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  return currentYear;
+}
+
 
 const Footer = () => {
   return <div className="bg-primary py-12 mt-12">
     <div>
-      <p className="text-white text-center">Copyright &copy; E-commerce Shop 2022. All rights Reserved  </p>
+      <p className="text-white text-center">Copyright &copy; E-commerce Shop {getCurrentYear()}. All rights Reserved  </p>
     </div>
   </div>;
 };
